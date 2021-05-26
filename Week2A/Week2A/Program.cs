0a77c111bc5e473600a2d1581e13d4d554c580ea
h-abokhadra <59623466+h-abokhadra@users.noreply.github.com>
2021-05-26 16:30:32 -0400

    Week2

---
+++ Week2A/Week2A/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Week2A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Hoda";
+            Console.WriteLine
+                ($"$Bingooo!{name} your name is hoda");
+
+            Thread.Sleep(8000);
+        }
+    }
+
+}
---
+++ Week2_lab/Week2_lab/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+
+            Console.Write("Enter first integer: ");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter 2nd ");
+            num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Sum is {num1 + num2}");
+
+            System.Threading.Thread.Sleep(5 * 60 * 1000);
+
+
+        }
+
+
+    }
+}
