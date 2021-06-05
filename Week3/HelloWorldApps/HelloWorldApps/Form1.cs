ace8145051b8fc784470b464ad7398d3a3e68653
h-abokhadra <59623466+h-abokhadra@users.noreply.github.com>
2021-06-04 21:39:22 -0400

    uploading labs

---
+++ Week3/HelloWorldApps/HelloWorldApps/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWorldApps
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Button event control with Click evnt
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (sender == button1)
+            {
+                label1.Text="Button 1 was clicked";
+                textBox1.Text = "Button 1 was clicked";
+            }
+
+            if (sender == button2)
+            {
+                label1.Text = "Button 2 was clicked";
+                textBox1.Text = "Button 2 was clicked";
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Do Nothing
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Form 1 was clicked");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Hello world";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
