9c1d4b17157ac623ba7e4f1e5364a41044dadc51
h-abokhadra <59623466+h-abokhadra@users.noreply.github.com>
2021-06-21 14:43:01 -0400

    test

--- Week3/HelloWorldApps/HelloWorldApps/Form1.Designer.cs
+++ Week3/HelloWorldApps/HelloWorldApps/Form1.Designer.cs
@@ -36,9 +36,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(363, 62);
+            this.button1.Location = new System.Drawing.Point(272, 43);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(135, 80);
+            this.button1.Size = new System.Drawing.Size(101, 65);
             this.button1.TabIndex = 0;
             this.button1.Text = "Show \"Hello Class\" Message";
             this.button1.UseVisualStyleBackColor = true;
@@ -46,9 +47,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(363, 297);
+            this.button2.Location = new System.Drawing.Point(272, 260);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(135, 76);
+            this.button2.Size = new System.Drawing.Size(101, 62);
             this.button2.TabIndex = 1;
             this.button2.Text = "Show \"Bye Class\" Message";
             this.button2.UseVisualStyleBackColor = true;
@@ -56,30 +58,33 @@
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(379, 198);
+            this.label1.Location = new System.Drawing.Point(284, 161);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(136, 34);
+            this.label1.Size = new System.Drawing.Size(102, 28);
             this.label1.TabIndex = 2;
             this.label1.Text = "Hello Class";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(382, 236);
+            this.textBox1.Location = new System.Drawing.Point(286, 192);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.Size = new System.Drawing.Size(76, 20);
             this.textBox1.TabIndex = 3;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(688, 469);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
