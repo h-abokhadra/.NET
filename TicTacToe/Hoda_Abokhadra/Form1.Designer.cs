b2688719285f7ba4e649dda1f04084eef7aea648
h-abokhadra <59623466+h-abokhadra@users.noreply.github.com>
2021-06-25 22:42:31 -0400

    Creating a TicTacToe Game! :D

---
+++ TicTacToe/Hoda_Abokhadra/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace Hoda_Abokhadra
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.x1 = new System.Windows.Forms.Button();
+            this.x3 = new System.Windows.Forms.Button();
+            this.x2 = new System.Windows.Forms.Button();
+            this.y2 = new System.Windows.Forms.Button();
+            this.y3 = new System.Windows.Forms.Button();
+            this.y1 = new System.Windows.Forms.Button();
+            this.z3 = new System.Windows.Forms.Button();
+            this.z2 = new System.Windows.Forms.Button();
+            this.z1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // x1
+            // 
+            this.x1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.x1.Font = new System.Drawing.Font("Microsoft Tai Le", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.x1.Location = new System.Drawing.Point(12, 12);
+            this.x1.Name = "x1";
+            this.x1.Size = new System.Drawing.Size(70, 70);
+            this.x1.TabIndex = 0;
+            this.x1.UseVisualStyleBackColor = false;
+            this.x1.Click += new System.EventHandler(this.button_click);
+            // 
+            // x3
+            // 
+            this.x3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.x3.Font = new System.Drawing.Font("Microsoft Tai Le", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.x3.Location = new System.Drawing.Point(200, 12);
+            this.x3.Name = "x3";
+            this.x3.Size = new System.Drawing.Size(70, 70);
+            this.x3.TabIndex = 1;
+            this.x3.UseVisualStyleBackColor = false;
+            this.x3.Click += new System.EventHandler(this.button_click);
+            // 
+            // x2
+            // 
+            this.x2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.x2.Font = new System.Drawing.Font("Microsoft Tai Le", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.x2.Location = new System.Drawing.Point(104, 12);
+            this.x2.Name = "x2";
+            this.x2.Size = new System.Drawing.Size(70, 70);
+            this.x2.TabIndex = 2;
+            this.x2.UseVisualStyleBackColor = false;
+            this.x2.Click += new System.EventHandler(this.button_click);
+            // 
+            // y2
+            // 
+            this.y2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.y2.Font = new System.Drawing.Font("Microsoft Tai Le", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.y2.Location = new System.Drawing.Point(104, 112);
+            this.y2.Name = "y2";
+            this.y2.Size = new System.Drawing.Size(70, 70);
+            this.y2.TabIndex = 3;
+            this.y2.UseVisualStyleBackColor = false;
+            this.y2.Click += new System.EventHandler(this.button_click);
+            // 
+            // y3
+            // 
+            this.y3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.y3.Font = new System.Drawing.Font("Microsoft Tai Le", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.y3.Location = new System.Drawing.Point(200, 112);
+            this.y3.Name = "y3";
+            this.y3.Size = new System.Drawing.Size(70, 70);
+            this.y3.TabIndex = 4;
+            this.y3.UseVisualStyleBackColor = false;
+            this.y3.Click += new System.EventHandler(this.button_click);
+            // 
+            // y1
+            // 
+            this.y1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.y1.Font = new System.Drawing.Font("Microsoft Tai Le", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.y1.Location = new System.Drawing.Point(12, 112);
+            this.y1.Name = "y1";
+            this.y1.Size = new System.Drawing.Size(70, 70);
+            this.y1.TabIndex = 5;
+            this.y1.UseVisualStyleBackColor = false;
+            this.y1.Click += new System.EventHandler(this.button_click);
+            // 
+            // z3
+            // 
+            this.z3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.z3.Font = new System.Drawing.Font("Microsoft Tai Le", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.z3.Location = new System.Drawing.Point(200, 212);
+            this.z3.Name = "z3";
+            this.z3.Size = new System.Drawing.Size(70, 70);
+            this.z3.TabIndex = 6;
+            this.z3.UseVisualStyleBackColor = false;
+            this.z3.Click += new System.EventHandler(this.button_click);
+            // 
+            // z2
+            // 
+            this.z2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.z2.Font = new System.Drawing.Font("Microsoft Tai Le", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.z2.Location = new System.Drawing.Point(104, 212);
+            this.z2.Name = "z2";
+            this.z2.Size = new System.Drawing.Size(70, 70);
+            this.z2.TabIndex = 7;
+            this.z2.UseVisualStyleBackColor = false;
+            this.z2.Click += new System.EventHandler(this.button_click);
+            // 
+            // z1
+            // 
+            this.z1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.z1.Font = new System.Drawing.Font("Microsoft Tai Le", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.z1.Location = new System.Drawing.Point(12, 212);
+            this.z1.Name = "z1";
+            this.z1.Size = new System.Drawing.Size(70, 70);
+            this.z1.TabIndex = 8;
+            this.z1.UseVisualStyleBackColor = false;
+            this.z1.Click += new System.EventHandler(this.button_click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(283, 293);
+            this.Controls.Add(this.z1);
+            this.Controls.Add(this.z2);
+            this.Controls.Add(this.z3);
+            this.Controls.Add(this.y1);
+            this.Controls.Add(this.y3);
+            this.Controls.Add(this.y2);
+            this.Controls.Add(this.x2);
+            this.Controls.Add(this.x3);
+            this.Controls.Add(this.x1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Tic Tac Toe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button x1;
+        private System.Windows.Forms.Button x3;
+        private System.Windows.Forms.Button x2;
+        private System.Windows.Forms.Button y2;
+        private System.Windows.Forms.Button y3;
+        private System.Windows.Forms.Button y1;
+        private System.Windows.Forms.Button z3;
+        private System.Windows.Forms.Button z2;
+        private System.Windows.Forms.Button z1;
+    }
+}
+
---
+++ TicTacToe/Hoda_Abokhadra/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//This program is for creating a tic tac toe game using forms, Author: Hoda Abokhadra, June 2021.
+
+namespace Hoda_Abokhadra
+{
+    public partial class Form1 : Form
+    {
+        //for counting the moves
+        int counter;
+        //True is for the 1st player turn while false would be for the 2nd player turn.
+        bool turnStatus = true; 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (turnStatus) //if true the turn would be for the 1st player
+            {
+                button.Text = "X";
+            }
+            else //if false the turn would be for the 2nd player
+            {
+                button.Text = "O";
+            }
+
+            turnStatus = !turnStatus; //For switching the turnStatus
+            button.Enabled = false;
+            counter++;
+
+            winnerCheck();
+        }
+
+        private void winnerCheck()
+        {
+            bool winner = false;
+            //Checking the rows wins
+            if((x1.Text == x2.Text)&&(x2.Text == x3.Text) && (!x1.Enabled)) //1st row - Disable the button after the win
+            {
+                winner = true;
+
+            }
+           else if ((y1.Text == y2.Text) && (y2.Text == y3.Text) && (!y1.Enabled)) //2nd row
+            {
+                winner = true;
+
+            }
+            else if ((z1.Text == z2.Text) && (z2.Text == z3.Text) && (!z1.Enabled)) //3rd row
+            {
+                winner = true;
+
+            }
+
+            //Checking the columns wins
+            else if ((x1.Text == y1.Text) && (y1.Text == z1.Text) && (!x1.Enabled)) //1st column - Disable the button after the win
+            {
+                winner = true;
+
+            }
+            else if ((x2.Text == y2.Text) && (y2.Text == z2.Text) && (!x2.Enabled)) //2nd column
+            {
+                winner = true;
+
+            }
+            else if ((x3.Text == y3.Text) && (y3.Text == z3.Text) && (!x3.Enabled)) //3rd column
+            {
+                winner = true;
+
+            }
+
+            //Checking the diagonal
+
+            else if ((x1.Text == y2.Text) && (y2.Text == z3.Text) && (!x1.Enabled)) //1st diagonal
+            {
+                winner = true;
+
+            }
+            else if ((x3.Text == y2.Text) && (y2.Text == z1.Text) && (!x3.Enabled)) //second diagonal
+            {
+                winner = true;
+
+            }
+            if (winner)
+            {
+                DisableButton();
+
+                String winnerIs = "";
+
+                if (turnStatus)
+
+                    winnerIs = "O";
+                
+                else
+                
+                    winnerIs = "X";
+                
+
+                MessageBox.Show(winnerIs + " IS THE WINNER!"); //Displaying the winner
+            }
+
+            else
+            {
+                if(counter == 9) //checking the draw status;
+                    MessageBox.Show("It's a draw!");
+            }
+
+        }
+        //A method for disableing the buttons
+        private void DisableButton()
+        {
+
+            foreach (Control control in Controls)
+            {
+
+                Button button = (Button)control;
+                button.Enabled = false;
+            }
+        }
+
+    }
+}
