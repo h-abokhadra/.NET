0a22e776285ad87c6fe583839c0c52aa2ef9173f
h-abokhadra <59623466+h-abokhadra@users.noreply.github.com>
2021-07-08 08:58:23 -0400

    Upload Calc. Project

---
+++ Week8/Interest Calculator/Interest Calculator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Interest_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double p = 1000;//princaple
+            double r = 0.05;//Rate
+
+            for (int n=1;n<=10;n++)
+            {
+                double a = p * (Math.Pow(1 + r, n));//Calculate the total after adding the intersest rate
+
+                Console.WriteLine(a);
+                Thread.Sleep(1000);
+            }
+            Thread.Sleep(6000);
+        }
+    }
+}
