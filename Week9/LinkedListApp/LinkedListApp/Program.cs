55881febdb2bd7fe83e5b23d4d4005c081054f59
h-abokhadra <59623466+h-abokhadra@users.noreply.github.com>
2021-07-16 10:28:18 -0400

    Class Lab

---
+++ Week9/LinkedListApp/LinkedListApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace LinkedListApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> EmpName = new LinkedList<string>();
+
+
+             //Mark Sarah Omar Paul Jeny
+
+            EmpName.AddFirst("Sarah");
+            EmpName.AddLast("Paul");
+            EmpName.AddFirst("Mark");
+
+            //Var that holds the found node
+            LinkedListNode<string> FoundNode= EmpName.Find("Paul");
+
+            //Add a node before paul's node
+            EmpName.AddBefore(FoundNode, "Omar");
+
+            //Add a node after paul's node
+            EmpName.AddAfter(FoundNode, "Jeny");
+
+
+            foreach (string name in EmpName)
+            {
+                Console.WriteLine($"The employee name is: {name}");
+                Thread.Sleep(1000);
+            }
+
+            Thread.Sleep(10000);
+        }
+    }
+}
