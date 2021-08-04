ac6c553712a9af21fcffe2be850527b78cae7b38
h-abokhadra <59623466+h-abokhadra@users.noreply.github.com>
2021-08-03 22:01:05 -0400

    Upload project

---
+++ Week10/AdoDataGridView/AdoDataGridView/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace AdoDataGridView
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Connection with the backend
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = @"Data Source = OMAR\OMARSQL; Initial Catalog = BooksDB; 
+                                                          Integrated Security = True";
+            //Open up the connection
+            con.Open();
+
+
+            //Query the data
+            //SP SQL command
+            SqlDataAdapter SQLCMD = new SqlDataAdapter("spGetTitleTwoPar", con);
+            SQLCMD.SelectCommand.CommandType = CommandType.StoredProcedure;
+            SQLCMD.SelectCommand.Parameters.Add("@EditionNumber", SqlDbType.VarChar).Value =textBox1.Text;
+            SQLCMD.SelectCommand.Parameters.Add("@Copyright", SqlDbType.VarChar).Value = textBox2.Text;
+
+            DataTable DT = new DataTable();
+            SQLCMD.Fill(DT);
+
+            //Present the data in a Data Grid View
+            dataGridView1.DataSource = DT;
+        }
+    }
+}
