4161772fd36911872bea19b54225c7f0bbf22520
h-abokhadra <59623466+h-abokhadra@users.noreply.github.com>
2021-08-03 22:12:50 -0400

    update

---
+++ Week10/WebConfigApp/WebConfigApp/HomePage.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="HomePage.aspx.cs" Inherits="WebConfigApp.HomePage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <p style="text-align: center">
+        <br />
+    </p>
+    <form id="form1" runat="server">
+        <p>
+            <asp:GridView ID="GridView1" runat="server">
+            </asp:GridView>
+        </p>
+        <p>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Show Data" />
+        </p>
+        <div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Week10/WebConfigApp/WebConfigApp/HomePage.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace WebConfigApp
+{
+    public partial class HomePage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+
+            /*Direct ADO connection
+            SqlConnection con = new SqlConnection(@"Data Source = OMAR\OMARSQL; Initial Catalog=BooksDB; 
+                                                         Integrated Security=True");*/
+
+            SqlConnection con = new SqlConnection(CS);
+            con.Open();
+
+
+            
+
+            SqlCommand SQLCMD = new SqlCommand("select * from Titles",con);
+            GridView1.DataSource = SQLCMD.ExecuteReader();
+            GridView1.DataBind();
+
+        }
+    }
+}
